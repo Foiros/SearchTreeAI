fdb599f59b0216ef0d9ed9b0ac0cccf4db8223ce
Foiros <arttu.p_90@hotmail.com>
2021-09-10 10:29:18 +0300

    1. General 10.9.2021: Actual Initial Commit

---
+++ cmake-build-debug/CMakeFiles/SearchTreeAI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SearchTreeAI.dir/main.cpp.obj"
+  "SearchTreeAI.exe"
+  "SearchTreeAI.exe.manifest"
+  "SearchTreeAI.lib"
+  "SearchTreeAI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SearchTreeAI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
